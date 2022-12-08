10159cc5216daccbf97524e68670304279f19dc9
Tachu <stitch_ditto_0j@icloud.com>
2022-12-08 16:45:44 +0900

    finish

---
+++ For02.java
@@ -0,0 +1,12 @@
+/**
+ * For02
+ */
+public class For02 {
+
+    public static void main(String[] args) {
+        int[] array = { 1,2,3,4,5 };
+        for (int number : array) {
+            System.out.println("For2 = " + number);
+        }
+    }
+}
