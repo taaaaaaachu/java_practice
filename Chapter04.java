39c2fa887f058eb50f6436d06efc2ec6a32007ce
Tachu <stitch_ditto_0j@icloud.com>
2022-12-08 15:22:18 +0900

    practice

---
+++ Chapter04.java
@@ -0,0 +1,16 @@
+/**
+ * Chapter04
+ */
+public class Chapter04 {
+
+    public static void main(String[] args) {
+        String name = "Tatsuki";
+        System.out.println(name);
+
+        name = "Ami";
+        System.out.println(name);
+
+        final int LUCKY_NUMBER = 708;
+        System.out.println(LUCKY_NUMBER);
+    }
+}
