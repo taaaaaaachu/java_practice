b8c30c6c85f2413cced268c0f9874c8af13d12e6
Tachu <stitch_ditto_0j@icloud.com>
2022-12-08 15:42:25 +0900

    all

---
+++ Chapter07.java
@@ -0,0 +1,19 @@
+/**
+ * Chapter07
+ */
+public class Chapter07 {
+
+    public static void main(String[] args) {
+        int far = 5;
+        if (far <= 5) {
+            System.out.println("とても近いです");
+        } else if (far <= 10) {
+            System.out.println("近いです");
+        } else if (far <= 15) {
+            System.out.println("遠いです");
+        } else {
+            System.out.println("とても遠いです");
+        }
+        
+    }
+}
